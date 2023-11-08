8af0e76cab1a5116b9a67628219092d429982273
Artsiom Zhalkouski <azhalkouski@gmail.com>
2023-11-08 18:05:37 +0300

    select option zero in transactions drop down

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-import sys
+from selenium.webdriver.common.by import By
+import sys, time
 
 driver = webdriver.Chrome()
 url = "https://selenium.dev"
@@ -8,4 +9,15 @@ if len(sys.argv) > 1:
   url = sys.argv[1]
 
 driver.get(url)
+
+acceptBtn = driver.find_element(By.ID, "onetrust-accept-btn-handler")
+acceptBtn.click()
+
+transactionsDropDown = driver.find_element(By.CSS_SELECTOR, sys.argv[2])
+transactionsDropDown.click()
+
+optionZero = driver.find_element(By.ID, sys.argv[3])
+optionZero.click()
+
+time.sleep(5)
 driver.quit()
