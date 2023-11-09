1faffd2e52877cdbf741a2bdaf4d1b73e7c6ebb0
Artsiom Zhalkouski <azhalkouski@gmail.com>
2023-11-09 13:48:55 +0300

    select specific location

--- index.py
+++ index.py
@@ -19,5 +19,24 @@ transactionsDropDown.click()
 optionZero = driver.find_element(By.ID, sys.argv[3])
 optionZero.click()
 
+locationBtn = driver.find_element(By.ID, sys.argv[4])
+locationBtn.click()
+driver.implicitly_wait(2)
+locationPicker = driver.find_element(By.ID, sys.argv[5])
+locationPicker.click()
+locationPicker.send_keys(sys.argv[6])
+driver.implicitly_wait(5)
+searchedLocationInput = driver.find_element(By.ID, sys.argv[7])
+searchedLocationLiElement = searchedLocationInput.find_element(By.XPATH, "./..")
+searchedLocationLiElement.click()
+
+# wait 5 sec because searchedLocationLiElement.click() initiales a network req
+# which then updates the search button text, which in turn embeds the prior
+# network req's response data
+time.sleep(5)
+searchBtn = driver.find_element(By.ID, sys.argv[8])
+
+print('searchBtn.text', searchBtn.text)
+
 time.sleep(5)
 driver.quit()
