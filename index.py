3325849dd546bd46cc5b60bb47b2f77a0de2182b
Artsiom Zhalkouski <azhalkouski@gmail.com>
2023-11-22 16:17:21 +0100

    wrap the scraping algorithm into a function

--- index.py
+++ index.py
@@ -6,46 +6,55 @@ from utils.index import openServiceConfig
 
 serviceConfig = openServiceConfig()
 
+def scrape_appartments_count_for_city(city_id=0, city_name=serviceConfig["cityName"]):
+    count = 0
 
-driver = webdriver.Chrome()
-url = serviceConfig["targetServiceUrl"]
-driver.get(url)
+    driver = webdriver.Chrome()
+    url = serviceConfig["targetServiceUrl"]
+    driver.get(url)
 
-acceptBtn = driver.find_element(By.ID, serviceConfig["acceptCookiesBtnId"])
-acceptBtn.click()
+    acceptBtn = driver.find_element(By.ID, serviceConfig["acceptCookiesBtnId"])
+    acceptBtn.click()
 
-transactionsDropDown = driver.find_element(
-    By.CSS_SELECTOR, serviceConfig["transactionTypeDropDownCssSelector"])
-transactionsDropDown.click()
+    transactionsDropDown = driver.find_element(
+        By.CSS_SELECTOR, serviceConfig["transactionTypeDropDownCssSelector"])
+    transactionsDropDown.click()
 
-optionZero = driver.find_element(
-    By.ID, serviceConfig["transactionTypeOprionZeroId"])
-optionZero.click()
+    optionZero = driver.find_element(
+        By.ID, serviceConfig["transactionTypeOprionZeroId"])
+    optionZero.click()
 
-locationBtn = driver.find_element(By.ID, serviceConfig["locationBtnId"])
-locationBtn.click()
-driver.implicitly_wait(2)
+    locationBtn = driver.find_element(By.ID, serviceConfig["locationBtnId"])
+    locationBtn.click()
+    driver.implicitly_wait(2)
 
-locationPicker = driver.find_element(
-    By.ID, serviceConfig["locationPickerId"])
-locationPicker.click()
-locationPicker.send_keys(serviceConfig["cityName"])
-driver.implicitly_wait(5)
+    locationPicker = driver.find_element(
+        By.ID, serviceConfig["locationPickerId"])
+    locationPicker.click()
+    locationPicker.send_keys(city_name)
+    driver.implicitly_wait(5)
 
-searchedLocationInput = driver.find_element(
-    By.ID, serviceConfig["dynamicallyMountedCityCheckboxId"])
-searchedLocationLiElement = searchedLocationInput.find_element(
-    By.XPATH, "./..")
-searchedLocationLiElement.click()
+    searchedLocationInput = driver.find_element(
+        By.ID, serviceConfig["dynamicallyMountedCityCheckboxId"])
+    searchedLocationLiElement = searchedLocationInput.find_element(
+        By.XPATH, "./..")
+    searchedLocationLiElement.click()
 
-# Wait for 5 seconds because searchedLocationLiElement.click() initiates
-# a network request which then updates the search button text,
-# which in turn embeds the prior network request's response data
-time.sleep(5)
+    # Wait for 5 seconds because searchedLocationLiElement.click() initiates
+    # a network request which then updates the search button text,
+    # which in turn embeds the prior network request's response data
+    time.sleep(5)
 
-searchBtn = driver.find_element(
-    By.ID, serviceConfig["searchFormSubmitBtnId"])
-print('searchBtn.text', searchBtn.text)
+    searchBtn = driver.find_element(
+        By.ID, serviceConfig["searchFormSubmitBtnId"])
+    print('searchBtn.text', searchBtn.text)
 
-time.sleep(5)
-driver.quit()
+    # TODO: extract_numeric_word(string_with_numeric_word)
+
+    driver.quit()
+
+    return count
+
+
+if __name__ == '__main__':
+    scrape_appartments_count_for_city()
