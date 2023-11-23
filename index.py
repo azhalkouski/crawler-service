44d6417c32e750df65ae661e3890f69560d2af92
Artsiom Zhalkouski <azhalkouski@gmail.com>
2023-11-23 16:36:57 +0100

    improve reliability of scraping for cities

--- index.py
+++ index.py
@@ -2,12 +2,34 @@ from services.scraper_service import ScraperService
 from services.db_service import DataBaseService
 
 
-dataBaseService = DataBaseService()
-scraperService = ScraperService()
+def process_cities(cities):
+    BREAKPOINT_COUNT = len(cities) + 5
+    iteration_count = 0
 
-cities = dataBaseService.get_all_cities()
+    while len(cities) > 0:
+        iteration_count += 1
+        city = cities.pop(0)
+        city_id, city_name = city
 
-for city in cities:
-    count = scraperService.scrape_appartments_count_for_city(city[1])
-    print(city[0], city[1], count)
-    dataBaseService.save_units_count(city[0], 'apartment', count)
+        try:
+            count_of_units = scraperService.scrape_appartments_count_for_city(city_name)
+            print(city_id, city_name, count_of_units)
+            dataBaseService.save_units_count(city[0], 'apartment', count_of_units)
+
+        except Exception as e:
+            cities.append(city)
+            print(f"Failed to scrape for {city_name} with an error: {e}")
+        
+            if iteration_count > BREAKPOINT_COUNT:
+                print(f"BREAKING THE CYCLE because of constantly failing to "
+                      f"scrape for {cities} with an error: {e}")
+                break
+
+
+if __name__ == '__main__':
+    dataBaseService = DataBaseService()
+    scraperService = ScraperService()
+
+    cities = dataBaseService.get_all_cities()
+
+    process_cities(cities)
