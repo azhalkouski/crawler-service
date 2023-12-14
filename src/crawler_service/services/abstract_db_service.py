53806570d43e7e1c74ada978942d5d991cfb63b8
Artsiom Zhalkouski <azhalkouski@gmail.com>
2023-12-14 23:00:55 +0100

    add transaction_type to  AbstractDBService save_units_count

--- src/crawler_service/services/abstract_db_service.py
+++ src/crawler_service/services/abstract_db_service.py
@@ -9,5 +9,7 @@ class AbstractDBService(abc.ABC):
         """Load all cities from DB"""
 
     @abc.abstractmethod
-    def save_units_count(self, city_id: int, unit_type: str, count: int):
+    def save_units_count(
+        self, city_id: int, unit_type: str, transaction_type: str, count: int
+    ):
         """Save units count to DB"""
