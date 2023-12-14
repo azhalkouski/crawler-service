fd9188c2192996c37d112659ecd029887dfd17b6
Artsiom Zhalkouski <azhalkouski@gmail.com>
2023-12-14 14:33:49 +0100

    take a screenshot of error occured during selenium's job

--- src/crawler_service/services/scraper_service.py
+++ src/crawler_service/services/scraper_service.py
@@ -12,6 +12,17 @@ from selenium.webdriver.common.by import By
 AbstractDBServiceT = TypeVar("AbstractDBServiceT", bound=AbstractDBService)
 
 
+def get_current_time():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+"""
+Respnsibility: obtain data and hand it over
+Should not: load cities from db, save data to db
+Should: be invoked with city, type_of_deal, type_of_unit passed via arguments
+"""
+
+
 class ScraperService:
     def __init__(self, db_service: AbstractDBServiceT):
         if not isinstance(db_service, AbstractDBService):
@@ -54,66 +65,81 @@ class ScraperService:
 
         driver = webdriver.Firefox(options=firefoxOptions, service=service)
         url = self.serviceConfig["targetServiceUrl"]
-        driver.get(url)
+        try:
+            driver.get(url)
 
-        driver.implicitly_wait(5)
-        acceptBtn = driver.find_element(By.ID, self.serviceConfig["acceptCookiesBtnId"])
-        acceptBtn.click()
+            driver.implicitly_wait(5)
+            acceptBtn = driver.find_element(
+                By.ID, self.serviceConfig["acceptCookiesBtnId"]
+            )
+            acceptBtn.click()
 
-        transactionsDropDown = driver.find_element(
-            By.CSS_SELECTOR, self.serviceConfig["transactionTypeDropDownCssSelector"]
-        )
-        transactionsDropDown.click()
+            transactionsDropDown = driver.find_element(
+                By.CSS_SELECTOR,
+                self.serviceConfig["transactionTypeDropDownCssSelector"],
+            )
+            transactionsDropDown.click()
 
-        optionZero = driver.find_element(
-            By.ID, self.serviceConfig["transactionTypeOprionZeroId"]
-        )
-        optionZero.click()
+            optionZero = driver.find_element(
+                By.ID, self.serviceConfig["transactionTypeOprionZeroId"]
+            )
+            optionZero.click()
 
-        locationBtn = driver.find_element(By.ID, self.serviceConfig["locationBtnId"])
-        locationBtn.click()
-        driver.implicitly_wait(2)
+            locationBtn = driver.find_element(
+                By.ID, self.serviceConfig["locationBtnId"]
+            )
+            locationBtn.click()
+            driver.implicitly_wait(2)
 
-        locationPicker = driver.find_element(
-            By.ID, self.serviceConfig["locationPickerId"]
-        )
-        locationPicker.click()
-        locationPicker.send_keys(city_name)
-        driver.implicitly_wait(5)
+            locationPicker = driver.find_element(
+                By.ID, self.serviceConfig["locationPickerId"]
+            )
+            locationPicker.click()
+            locationPicker.send_keys(city_name)
+            driver.implicitly_wait(5)
 
-        searchedLocationInput = driver.find_element(
-            By.ID, self.serviceConfig["dynamicallyMountedCityCheckboxIds"][city_name]
-        )
-        driver.implicitly_wait(5)
-        lishka = searchedLocationInput.find_element(By.XPATH, "./..")
-        label = lishka.find_element(
-            By.XPATH,
-            f"//label[@for='{self.serviceConfig['dynamicallyMountedCityCheckboxIds'][city_name]}']",
-        )
-        label.click()
+            searchedLocationInput = driver.find_element(
+                By.ID,
+                self.serviceConfig["dynamicallyMountedCityCheckboxIds"][city_name],
+            )
+            driver.implicitly_wait(5)
+            lishka = searchedLocationInput.find_element(By.XPATH, "./..")
+            label = lishka.find_element(
+                By.XPATH,
+                f"//label[@for='{self.serviceConfig['dynamicallyMountedCityCheckboxIds'][city_name]}']",
+            )
+            label.click()
 
-        # Wait for 5 seconds because searchedLocationLiElement.click() initiates
-        # a network request which then updates the search button text,
-        # which in turn embeds the prior network request's response data
-        # TODO: get rid of this wait. 5 sec is way too long
-        time.sleep(3)
+            # Wait for 5 seconds because searchedLocationLiElement.click() initiates
+            # a network request which then updates the search button text,
+            # which in turn embeds the prior network request's response data
+            # TODO: get rid of this wait. 5 sec is way too long
+            time.sleep(3)
 
-        searchBtn = driver.find_element(
-            By.ID, self.serviceConfig["searchFormSubmitBtnId"]
-        )
+            searchBtn = driver.find_element(
+                By.ID, self.serviceConfig["searchFormSubmitBtnId"]
+            )
 
-        count = (
-            extract_numeric_word(searchBtn.text)
-            if extract_numeric_word(searchBtn.text) is not None
-            else 0
-        )
+            count = (
+                extract_numeric_word(searchBtn.text)
+                if extract_numeric_word(searchBtn.text) is not None
+                else 0
+            )
 
-        driver.quit()
+        except Exception as e:
+            driver.save_screenshot(f"possible-ip-block-{get_current_time()}.png")
+            self.error_logger.error(
+                f"Failed to scrape for {city_name} with an error: {e}"
+            )
+            # buble up the exception to the caller
+            raise e
+        else:
+            driver.quit()
 
         return count
 
     def process_cities(self):
-        print(f'processing at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+        print(f"processing at {get_current_time()}")
         if len(self.cities) == 0:
             return None
 
